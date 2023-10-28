12dd98a198fa21c2d39df41489a2ebc2fd2987a3
WasTabon <titanium.jen@gmail.com>
2023-10-28 17:04:48 +0300

    finished

--- app/src/main/java/com/example/testone/DescActivity.kt
+++ app/src/main/java/com/example/testone/DescActivity.kt
@@ -2,10 +2,19 @@ package com.example.testone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class DescActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_desc)
+
+        val args = intent.extras
+
+        val textName = findViewById<TextView>(R.id.catName)
+        val textDesc = findViewById<TextView>(R.id.catDescription)
+
+        textName.text = args?.getString("catName")
+        textDesc.text = args?.getString("catDescription")
     }
 }
--- app/src/main/java/com/example/testone/MainActivity.kt
+++ app/src/main/java/com/example/testone/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.VibrationAttributes
+import android.view.View.OnClickListener
 import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,16 +20,27 @@ class MainActivity : AppCompatActivity() {
         val Ben = Cat(getString(R.string.cat_name_Ben), getString(R.string.cat_description_Ben), R.drawable.baseline_sanitizer)
         val Bin = Cat(getString(R.string.cat_name_Bin), getString(R.string.cat_description_Bin), R.drawable.baseline_sanitizer)
 
-        val buttonDetailsBon = findViewById<Button>(R.id.CatOneDetails)
-        val buttonDetailsBen = findViewById<Button>(R.id.CatTwoDetails)
+        val buttonDetailsBon = findViewById<Button>(R.id.CatTwoDetails)
+        val buttonDetailsBen = findViewById<Button>(R.id.CatOneDetails)
         val buttonDetailsBin = findViewById<Button>(R.id.CatThreeDetails)
 
-        buttonDetailsBon.setOnClickListener {
-            val intent = Intent(this, DescActivity::class.java)
-            startActivity(intent)
+        buttonDetailsBon.setOnClickListener{
+            LoadDescription(getString(R.string.cat_name_Bon), getString(R.string.cat_description_Bon))
+        }
+        buttonDetailsBen.setOnClickListener{
+            LoadDescription(getString(R.string.cat_name_Ben), getString(R.string.cat_description_Ben))
+        }
+        buttonDetailsBin.setOnClickListener{
+            LoadDescription(getString(R.string.cat_name_Bin), getString(R.string.cat_description_Bin))
         }
     }
 
-    
+    fun LoadDescription(name: String, description: String)
+    {
+        val intent = Intent(this, DescActivity::class.java)
+        intent.putExtra("catName", name)
+        intent.putExtra("catDescription", description)
+        startActivity(intent)
+    }
 
 }
